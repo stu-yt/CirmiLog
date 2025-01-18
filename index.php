3ddc7f449d969d506d5a25435c246070549beb74
Stu <37384771+stu-yt@users.noreply.github.com>
2025-01-18 19:55:26 +0100

    Initial UI tweaks.

--- index.php
+++ index.php
@@ -8,13 +8,14 @@ include 'templates/header.php';
 ?>
 
 <div class="container">
-    <h1>Simple PHP Forum</h1>
-    <nav>
-        <a href="index.php?status=open">Open Posts</a> |
-        <a href="index.php?status=closed">Closed Posts</a> |
+    <h1>Projects</h1>
+    <nav class="typeSelector">
+        <a href="index.php?status=open">Open Posts</a> 
+        <a href="index.php?status=closed">Closed Posts</a> 
         <a href="index.php?status=all">All Posts</a>
     </nav>
 
+    
     <h2>Create a New Post</h2>
     <form method="POST" action="forum.php">
         <input type="hidden" name="action" value="add_post">
@@ -35,7 +36,7 @@ include 'templates/header.php';
                 <p><?php echo nl2br(htmlspecialchars($post['content'])); ?></p>
                 <p>Status: <?php echo $post['status']; ?></p>
 
-                <form method="POST" action="forum.php" style="display:inline;">
+                <form method="POST" action="forum.php">
                     <input type="hidden" name="action" value="change_status">
                     <input type="hidden" name="post_id" value="<?php echo $post['id']; ?>">
                     <input type="hidden" name="new_status" value="<?php echo $post['status'] === 'open' ? 'closed' : 'open'; ?>">
